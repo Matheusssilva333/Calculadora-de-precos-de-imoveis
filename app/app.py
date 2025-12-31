268b5f435e4e167d5d4c278ba8b5913a4a8a5f5d
Matheus Silva <matheus1882014@outlook.com>
2025-12-31 17:54:55 -0300

    ajustando detalhes iniciais do código

--- app/app.py
+++ app/app.py
@@ -3,6 +3,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-def barra de pesquisa(pesquisar-endereço):
+@app.route('/index.html') # Define a rota para a página inicial
+def home():
+    # Retorna o template HTML chamado 'index.html'
+    # Flask procura por este arquivo na pasta 'templates'
+    return render_template('index.html')
 
-@app.route('/endereço')
+    if __name__ == '__main__':
+    app.run(debug=True)
+
