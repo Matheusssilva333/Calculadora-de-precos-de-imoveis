a06866fa2902b2ece0a2fb09389954287e328268
Matheus Silva <matheus1882014@outlook.com>
2026-01-21 10:52:47 -0300

    ajustando rotas Python Flask

--- app/Back End/app.py
+++ app/Back End/app.py
@@ -12,14 +12,21 @@ import matplotlib
 
 import pandas as pd
 
+import tkinter as tk
+
 
 app = Flask(__name__)
 
+# Rota da página inicial
 
 @app.route('/') 
-def home():
-    
-    return render_template('index.html')
+def home(): return "Seja bem vindo!"
+print ("Clique aqui")
+
+# Rota Página de pesquisa de preços
+@app.route('/index.html')
+def Cálculo_de_preços () : return "index.html"
+
 
     if __name__ == '__main__':
     app.run(debug=True)
