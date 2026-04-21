c1664ce4f0a679914028bb6c4ee310877172553f
Matheus Silva <matheus1882014@outlook.com>
2026-04-21 14:42:45 -0300

    adicionando ponto e virgula que faltavam nas funções

--- app/app.py
+++ app/app.py
@@ -3,13 +3,17 @@ class BarraDePesquisa
 
 def __init__ (self);
 self.altura = 3 cm
+
 self.largura = 5 cm
+
 self.comprimento = 5 cm
 
-def titulo (self)
+def titulo (self);
+
 self.print(Pesquise aqui)
 
-def pesquisar (self)
+def pesquisar (self);
+
 self.print("Digite aqui")
 
 barra_de_pesquisa = BarraDePesquisa
