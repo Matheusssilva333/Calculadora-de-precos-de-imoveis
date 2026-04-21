e9999a147419c9131114d4320fac08617aa0fecd
Matheus Silva <matheus1882014@outlook.com>
2026-04-21 14:47:04 -0300

    adicionando atributos no objeto titulo

--- app/app.py
+++ app/app.py
@@ -1,22 +1,38 @@
  
 class BarraDePesquisa
 
+
 def __init__ (self);
+
 self.altura = 3 cm
 
 self.largura = 5 cm
 
 self.comprimento = 5 cm
 
+
+
 def titulo (self);
 
+
+self.formato = texto
+
+self.altura = 1 cm
+
+self.largura = 3 cm
+
+self.comprimento = 3 cm
+
 self.print(Pesquise aqui)
+
+
 
 def pesquisar (self);
 
 self.print("Digite aqui")
 
 barra_de_pesquisa = BarraDePesquisa
+
 print(barra_de_pesquisa.titulo.pesquisar)
 
 
