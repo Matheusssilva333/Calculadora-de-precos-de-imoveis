6b34644d64c251b8db74c4824c58a28c9c673211
Matheus Silva <matheus1882014@outlook.com>
2026-04-21 14:22:41 -0300

    Criando barra de pesquisa

---
+++ app/app.py
@@ -0,0 +1,13 @@
+
+class BarraDePesquisa
+
+def __init__(self);
+self.altura = 3 cm
+self.largura = 5 cm
+self.comprimento = 5 cm
+
+def Pesquisar(self)
+self.print("Digite aqui")
+
+Barra_De_Pesquisa = BarraDePesquisa
+print(Barra_De_Pesquisa.Pesquisar)
