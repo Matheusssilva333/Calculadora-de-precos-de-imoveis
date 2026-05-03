f302bd8aecb008611bcc98d5d031365382382583
Matheus de Souza Silva <matheus1882014@outlook.com>
2026-05-03 20:58:00 -0300

    criando e implementando um agente de IA utilizando o agno

--- app/app.py
+++ app/app.py
@@ -1,19 +1,18 @@
-
-    // Interface do sistema 
-
-
-    class BarraDePesquisa:  
+from agno.agent import Agent  
+   
+  agent = Agent(id="Consultor de investimentos", name="Agente de Investimentos", role="Responda perguntas se baseando em fontes cientifícas e acadêmicas confiáveis", instructions=["Busque as principais fontes confiáveis sobre o problema","Entregue uma saída concisa explicando sobre o assunto do problema","Não invente links, priorize sites oficiais, artigos renomados e referências robustas", ])
+     class BarraDePesquisa:  
 
 
     def __init__(self):
 
-        self.cor = preto    
+        self.cor = 0x000  
 
-        self.altura = 3 cm  
+        self.altura = 3  
 
-        self.largura = 5 cm 
+        self.largura = 5 
 
-        self.comprimento = 5 cm 
+        self.comprimento = 5 
 
 
 
--- app/logic.py
+++
@@ -1,2 +0,0 @@
-
-// Lógica de cálculo do sistema
