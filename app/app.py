0ef8f676070e9d51930f9f144453cd4bbc0d62af
Matheus de Souza Silva <matheus1882014@outlook.com>
2026-05-04 11:14:32 -0300

    resolvendo identação

--- app/app.py
+++ app/app.py
@@ -1,28 +1,27 @@
- 
-class BarraDePesquisa:  
+class BarraDePesquisa:
 
 
 
    def __init__(self):
 
-        self.cor = 0x000  
+        self.cor = 0x000
 
-        self.altura = 3  
+        self.altura = 3
 
-        self.largura = 5 
+        self.largura = 5
 
-        self.comprimento = 5 
-
-
+        self.comprimento = 5
 
 
-    def pesquisar(self):   
 
-        self.print("Digite aqui")   
 
-        barra_de_pesquisa = BarraDePesquisa 
+def pesquisar(self):
 
-        print(barra_de_pesquisa.pesquisar)  
+        self.print("Digite aqui")
+
+        barra_de_pesquisa = BarraDePesquisa
+
+        print(barra_de_pesquisa.pesquisar)
 
 
 
